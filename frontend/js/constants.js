2af9e4cfa81e2329fdce44ca2f4cd3ad11b23586
siaujunyu <siaujunyu@gmail.com>
2022-05-10 13:49:54 +0800

    Rinkeby Test

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,4 +1,4 @@
-const contractAddress = "YOUR CONTRACT ADDRESS"; //Replace with your own contract address
+const contractAddress = "0x98ac71fEc745ce35533C5a7BDEC7259f16Ae1674"; //Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby or polygon
 
 const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
